498d7200a1815a26ef3937dbaa9d5628b3ee943c
Leniorko <www.Len244@gmail.com>
2021-10-04 01:01:03 +0300

    Fixed typo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,29 +13,29 @@ class AdminController extends Controller
 
         if ($request->isMethod("POST")) {
             if ($request->decision === "resolved") {
-                $resolver_ticket = Ticket::where("id", $request->id)->get()[0];
+                $resolved_ticket = Ticket::where("id", $request->id)->get()[0];
 
                 $username = auth()->user()->login;
-                $ticketCreationDate = $resolver_ticket->created_at;
+                $ticketCreationDate = $resolved_ticket->created_at;
                 $new_img_name = "solution_$username$ticketCreationDate";
 
                 $request->file('solution_image')->storeAs("public/solutions", $new_img_name);
 
                 // Storage::url() not working perfectly. So seted up it by hand
                 $image_url = "/storage/solutions/$new_img_name";
-                $resolver_ticket->solution_img_link = $image_url;
-                $resolver_ticket->status = $request->decision;
-                $resolver_ticket->save();
-                $resolver_ticket->refresh();
+                $resolved_ticket->solution_img_link = $image_url;
+                $resolved_ticket->status = $request->decision;
+                $resolved_ticket->save();
+                $resolved_ticket->refresh();
 
                 return view("profile.admin", ["tickets" => $every_ticket]);
             }
 
             if ($request->decision === "rejected") {
-                $resolver_ticket = Ticket::where("id", $request->id)->get()[0];
+                $resolved_ticket = Ticket::where("id", $request->id)->get()[0];
 
-                $resolver_ticket->status = $request->decision;
-                $resolver_ticket->save();
+                $resolved_ticket->status = $request->decision;
+                $resolved_ticket->save();
                 return view("profile.admin", ["tickets" => $every_ticket]);
             }
         }
